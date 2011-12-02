0d5c102f7d563a3b6810ea9cf7c5756e46c76327
Charles Couillard <couillardcharles@gmail.com>
2011-12-02 20:37:02 +0100

    Changement de l'adresse de la repository, utilisation de l'adresse https Ajout controle sur le nombre de parametres (1, le nom du kata) Suppression de la demande de validation par maven à la création du projet

--- script/nouveau_workspace.sh
+++ script/nouveau_workspace.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 ECLIPSE="/Applications/eclipse/eclipse"
-REPOGIT="git@github.com:BodySplash/Eclipse-settings.git"
+REPOGIT="https://github.com/BodySplash/Archetype-Kata.git"
 GROUP_ARCHETYPE="fr.arpinum"
 ARTIFACT_ARCHETYPE="kata"
 NOM_PROJET=$1
 WORKSPACE="$PWD"
+
+function assure_presence_parametre() {
+   if [ $# -ne 1 ]
+   then
+       echo "Usage: `basename $0` {nomKata}"
+       exit 65
+   fi
+}
 
 function creer_workspace () {
 	git clone $REPOGIT ./
@@ -16,9 +24,10 @@ function ouvrir_eclipse () {
 }
 
 function creer_projet () {
-	mvn archetype:generate -DgroupId=fr.arpinum -DartifactId=$NOM_PROJET -Dversion=1.0.0-SNAPSHOT -DarchetypeGroupId=$GROUP_ARCHETYPE -DarchetypeArtifactId=$ARTIFACT_ARCHETYPE
+	mvn archetype:generate -DgroupId=fr.arpinum -DartifactId=$NOM_PROJET -Dversion=1.0.0-SNAPSHOT -DarchetypeGroupId=$GROUP_ARCHETYPE -DarchetypeArtifactId=$ARTIFACT_ARCHETYPE -DinteractiveMode=false
 }
-                         
+
+assure_presence_parametre $@                        
 creer_workspace
 creer_projet
 ouvrir_eclipse
